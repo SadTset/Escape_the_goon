82daab004fcb87f00fe887405832a916010c62a1
SadTset <tenzin.sharlhey@lernende.bbw.ch>
2026-05-21 11:30:08 +0200

    Add core game classes and tests for MovableSprite and PixelSprite
    - Introduced MovableSprite and PixelSprite classes to handle sprite movement and pixel drawing.
    - Implemented Starfield and Star classes for starfield generation.
    - Created GameBbwoy class to manage game logic and rendering.
    - Added SwingController and SwingDisplay for GUI handling.
    - Developed unit tests for ExampleDrawingTest and MoveableSpriteTest to validate functionality.
    - Included MockGraphic class for testing graphical output.

---
+++ Escape_the_Goon.java
@@ -0,0 +1,14 @@
+import ch.bbw.gamebbwoy.api.ButtonListener;
+import ch.bbw.gamebbwoy.api.PixelDisplay;
+import ch.bbw.gamebbwoy.api.PixelDrawing;
+import ch.bbw.gamebbwoy.internal.GameBbwoy;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Escape_the_Goon implements PixelDrawing {
+    public void tick(PixelDisplay graphic) {
+        for (var )
+    }
+
+}

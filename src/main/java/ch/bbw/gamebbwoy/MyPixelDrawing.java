6410c10862bd1872abcc4e05915e720d099a3e10
SadTset <tenzin.sharlhey@lernende.bbw.ch>
2026-06-02 08:21:01 +0200

    feat: Make hallway arrays public static for accessibility in backgrounds class

--- src/main/java/ch/bbw/gamebbwoy/MyPixelDrawing.java
+++ src/main/java/ch/bbw/gamebbwoy/MyPixelDrawing.java
@@ -26,9 +26,13 @@ public class MyPixelDrawing implements PixelDrawing, ButtonListener {
 			activeHallway = backgrounds.hallway;
 		} else if (hallwayNumber == 1) {
 			activeHallway = backgrounds.hallway2;
-		} else {
+		} else if (hallwayNumber == 2){
 			activeHallway = backgrounds.hallway3;
-		} 
+		} else if (hallwayNumber == 3) {
+			activeHallway = backgrounds.hallwayRight;
+		} else if (hallwayNumber == 4) {
+			activeHallway = backgrounds.hallwayLeft;
+		}
 		drawScaledSprite(graphic, activeHallway, 0, 0, graphic.getPixelWidth(), graphic.getPixelHeight());
 
 	}
@@ -37,6 +41,8 @@ public class MyPixelDrawing implements PixelDrawing, ButtonListener {
 	public void onButtonPress(ButtonListener.GameButton button) {
 		if (button == ButtonListener.GameButton.UP) {
 			hallwayNumber = 1;
+		} else if (button == ButtonListener.GameButton.LEFT) {
+			hallwayNumber = 
 		}
 	}
 

d2df1ca7c1d6bcced817481bc3a0f5a12db0d262
Tseten Sharlhey <tseten.sharlhey@santismail.ch>
2026-06-08 07:58:43 +0200

    finished for now

--- src/main/java/ch/bbw/gamebbwoy/MyPixelDrawing.java
+++ src/main/java/ch/bbw/gamebbwoy/MyPixelDrawing.java
@@ -7,12 +7,12 @@ import ch.bbw.gamebbwoy.Classes.HallwayLeft;
 import ch.bbw.gamebbwoy.Classes.HallwayRight;
 import ch.bbw.gamebbwoy.Classes.desk;
 import ch.bbw.gamebbwoy.Classes.everythingelse;
+import ch.bbw.gamebbwoy.Classes.props;
 import ch.bbw.gamebbwoy.api.ButtonListener;
 import ch.bbw.gamebbwoy.api.PixelColor;
 import ch.bbw.gamebbwoy.api.PixelDisplay;
 import ch.bbw.gamebbwoy.api.PixelDrawing;
 import ch.bbw.gamebbwoy.internal.GameBbwoy;
-import ch.bbw.gamebbwoy.Classes.props;
 
 public class MyPixelDrawing implements PixelDrawing, ButtonListener {
 	private int backgroundNumber = 0;
@@ -53,6 +53,10 @@ public class MyPixelDrawing implements PixelDrawing, ButtonListener {
 		case 8 -> everythingelse.blackout;
 		case 9 -> props.leverOff;
 		case 10 -> props.leverOn;
+		case 11 -> everythingelse.theGoon;
+		case 12 -> everythingelse.gameOver;
+		case 13 -> desk.deskEnd;
+		case 14 -> everythingelse.uWon;
         default -> Hallway.hallway;
     };
 
@@ -66,8 +70,7 @@ public class MyPixelDrawing implements PixelDrawing, ButtonListener {
     );
 }
 
-
-	@Override
+@Override
 public void onButtonPress(ButtonListener.GameButton button) {
 
     if (titleScreen && button == ButtonListener.GameButton.SPACE) {
@@ -77,7 +80,12 @@ public void onButtonPress(ButtonListener.GameButton button) {
     }
 
     if (backgroundNumber == 7 && button == ButtonListener.GameButton.SPACE) {
-        backgroundNumber = 8;
+        if (powerOn) {
+            backgroundNumber = 13;
+        } else {
+            hallwayHistory.push(backgroundNumber);
+            backgroundNumber = 8;
+        }
         return;
     }
 
@@ -92,21 +100,48 @@ public void onButtonPress(ButtonListener.GameButton button) {
         return;
     }
 
-    if (button == ButtonListener.GameButton.DOWN) {
-        if (!hallwayHistory.isEmpty()) {
-            backgroundNumber = hallwayHistory.pop();
-        }
+    if (backgroundNumber == 11 && button == ButtonListener.GameButton.SPACE) {
+        backgroundNumber = 12;
         return;
     }
 
+    if (backgroundNumber == 13 && button == ButtonListener.GameButton.SPACE) {
+        backgroundNumber = 14;
+        return;
+    }
+
+    if ((backgroundNumber == 12 || backgroundNumber == 14) && button == ButtonListener.GameButton.SPACE) {
+        titleScreen = true;
+        backgroundNumber = 0;
+        powerOn = false;
+        hallwayHistory.clear();
+        return;
+    }
+
+   if (button == ButtonListener.GameButton.DOWN) {
+    if (!hallwayHistory.isEmpty()) {
+        backgroundNumber = hallwayHistory.pop();
+
+        if (backgroundNumber == 7 && powerOn) {
+            backgroundNumber = 13;
+        }
+    }
+    return;
+}
+
     boolean canMove =
             (button == ButtonListener.GameButton.UP && (backgroundNumber == 0 || backgroundNumber == 1 || backgroundNumber == 2))
-            || (button == ButtonListener.GameButton.RIGHT && backgroundNumber == 3)
-            || (button == ButtonListener.GameButton.LEFT && backgroundNumber == 4)
-            || ((button == ButtonListener.GameButton.LEFT || button == ButtonListener.GameButton.RIGHT) && backgroundNumber == 5);
+                    || (button == ButtonListener.GameButton.RIGHT && backgroundNumber == 3)
+                    || (button == ButtonListener.GameButton.LEFT && backgroundNumber == 4)
+                    || ((button == ButtonListener.GameButton.LEFT || button == ButtonListener.GameButton.RIGHT) && backgroundNumber == 5);
 
     if (canMove) {
         hallwayHistory.push(backgroundNumber);
+
+        if (hallwayRandom.nextInt(12) == 0) {
+            backgroundNumber = 11;
+            return;
+        }
 
         int randomBackground = hallwayRandom.nextInt(powerOn ? 7 : 8);
 
